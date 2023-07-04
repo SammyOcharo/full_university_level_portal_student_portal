c2089a9e3dc038b297c5b87f52a243bbd50ee32f
sammy ocharo <obanyisammy@gmail.com>
2023-07-04 11:46:10 +0300

    initialize auth app endpoint

--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+
 from authentication.serializers import StudentLoginSerializer
-# Create your views here.
 
 class StudentLoginAPIView(APIView):
     serializer_class = StudentLoginSerializer
