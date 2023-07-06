fd57452b23fbad1315c28c4bc8d6be6475ddb724
sammy ocharo <obanyisammy@gmail.com>
2023-07-06 08:12:19 +0300

    complete login and verify login endpoint

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('student-login/', views.StudentLoginAPIView.as_view(), name='student-login-api'),
-    path('admin-verify-login/', views.StudentVerifyLoginAPIView.as_view(), name='verify-student-login-api'),
+    path('student-verify-login/', views.StudentVerifyLoginAPIView.as_view(), name='verify-student-login-api'),
 ]
