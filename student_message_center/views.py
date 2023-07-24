5bfab602f2037fdc70d143ac8c7bc6b8a4f99852
sammy ocharo <obanyisammy@gmail.com>
2023-07-24 07:18:54 +0300

    add messages endpoint

--- student_message_center/views.py
+++ student_message_center/views.py
@@ -1,4 +1,22 @@
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+
+from student_message_center.serializers import StudentDeleteMessageSerializer, StudentDeleteThreadSerializer, StudentSendMessageSerializer, StudentViewMessageSerializer
 
 class StudentViewMessageAPIView(APIView):
-    pass
+    permission_classes = [IsAuthenticated]
+    serializer_class = StudentViewMessageSerializer
+
+class StudentSendMessageAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = StudentSendMessageSerializer
+
+class StudentDeleteMessageAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = StudentDeleteMessageSerializer
+
+
+class StudentDeleteThreadAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = StudentDeleteThreadSerializer
+
